ceb61e9d38689fe9145eefeca102c727c43e6e59
Laurent Laborde <kerdezixe@gmail.com>
2023-01-28 09:08:16 +0100

    converting Simulation to kotlin

--- src/main/java/protoevo/core/Simulation.kt
+++ src/main/java/protoevo/core/Simulation.kt
@@ -1,281 +1,255 @@
-package protoevo.core;
+package protoevo.core
 
-import org.yaml.snakeyaml.Yaml;
-import org.yaml.snakeyaml.constructor.Constructor;
-import protoevo.biology.MeatCell;
-import protoevo.biology.PlantCell;
-import protoevo.biology.Protozoan;
-import com.github.javafaker.Faker;
-import protoevo.env.Tank;
-import protoevo.utils.FileIO;
-import protoevo.utils.Utils;
+import com.github.javafaker.Faker
+import protoevo.biology.MeatCell
+import protoevo.biology.PlantCell
+import protoevo.biology.Protozoan
+import protoevo.env.Tank
+import protoevo.utils.FileIO.appendLine
+import protoevo.utils.FileIO.load
+import protoevo.utils.FileIO.save
+import protoevo.utils.Utils.timeSeconds
+import java.io.File
+import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.stream.Collectors
 
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.text.SimpleDateFormat;
-import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+class Simulation {
+    var tank: Tank? = null
+        private set
+    private var simulate: Boolean
+    private var pause = false
+    var timeDilation = 1f
+    private var timeSinceSave = 0f
+    private var timeSinceSnapshot = 0f
+    private var updateDelay = Application.refreshDelay / 1000.0
+    private var lastUpdateTime = 0.0
+    private var debug = false
+    private var delayUpdate = true
+    private val name: String
+    private val genomeFile: String
+    private val historyFile: String
+    private var statsNames: List<String>? = null
 
-public class Simulation
-{
+    constructor() {
+        simulate = true
+        name = generateSimName()
+        println("Created new simulation named: $name")
+        genomeFile = "saves/$name/genomes.csv"
+        historyFile = "saves/$name/history.csv"
+        settingsPath = "saves/$name/settings.yaml"
+        newSaveDir()
+        newDefaultTank()
+        loadSettings()
+        RANDOM = Random(Settings.simulationSeed)
+    }
 
-	public static String defaultSettingsPath = "config/default_settings.yaml";
-	public static String settingsPath = defaultSettingsPath;
+    constructor(name: String) {
+        simulate = true
+        this.name = name
+        genomeFile = "saves/$name/genomes.csv"
+        historyFile = "saves/$name/history.csv"
+        settingsPath = "saves/$name/settings.yaml"
+        newSaveDir()
+        loadMostRecentTank()
+        loadSettings()
+        RANDOM = Random(Settings.simulationSeed)
+    }
 
-	private Tank tank;
-	private boolean simulate, pause = false;
-	private float timeDilation = 1, timeSinceSave = 0, timeSinceSnapshot = 0;
-	private double updateDelay = Application.refreshDelay / 1000.0, lastUpdateTime = 0;
-	
-	public static Random RANDOM;
-	private boolean debug = false, delayUpdate = true;
+    constructor(name: String, save: String) {
+        simulate = true
+        this.name = name
+        genomeFile = "saves/$name/genomes.csv"
+        historyFile = "saves/$name/history.csv"
+        newSaveDir()
+        loadTank("saves/$name/tank/$save")
+        loadSettings()
+        RANDOM = Random(Settings.simulationSeed)
+    }
 
-	private final String name;
-	private final String genomeFile, historyFile;
-	private List<String> statsNames;
+    private fun loadSettings() {
+        tank!!.cellCapacities[Protozoan::class.java] = Settings.maxProtozoa
+        tank!!.cellCapacities[PlantCell::class.java] = Settings.maxPlants
+        tank!!.cellCapacities[MeatCell::class.java] = Settings.maxMeat
+    }
 
-	public Simulation()
-	{
-		simulate = true;
-		name = generateSimName();
-		System.out.println("Created new simulation named: " + name);
-		genomeFile = "saves/" + name + "/genomes.csv";
-		historyFile = "saves/" + name + "/history.csv";
-		settingsPath = "saves/" + name + "/settings.yaml";
-		newSaveDir();
-		newDefaultTank();
-		loadSettings();
-		RANDOM = new Random(Settings.simulationSeed);
-	}
+    private fun newSaveDir() {
+        try {
+            val saveDir = Paths.get("saves/$name")
+            if (!Files.exists(saveDir)) {
+                Files.createDirectories(saveDir)
+                Files.createDirectories(Paths.get("saves/$name/tank"))
+                val original = File(defaultSettingsPath)
+                val copied = File(settingsPath)
+                com.google.common.io.Files.copy(original, copied)
+            }
+            val genomePath = Paths.get(genomeFile)
+            if (!Files.exists(genomePath)) Files.createFile(genomePath)
+            val historyPath = Paths.get(historyFile)
+            if (!Files.exists(historyPath)) Files.createFile(historyPath)
+            val seedFile = "saves/$name/seed.txt"
+            val seedPath = Paths.get(seedFile)
+            if (!Files.exists(seedPath)) {
+                try {
+                    Files.createFile(seedPath)
+                    appendLine(seedFile, Settings.simulationSeed.toString() + "")
+                } catch (e: IOException) {
+                    println("Failed to create seed file.")
+                }
+            }
+        } catch (e: IOException) {
+            throw RuntimeException(e)
+        }
+    }
 
-	public Simulation(String name)
-	{
-		simulate = true;
-		this.name = name;
-		genomeFile = "saves/" + name + "/genomes.csv";
-		historyFile = "saves/" + name + "/history.csv";
-		settingsPath = "saves/" + name + "/settings.yaml";
+    fun newDefaultTank() {
+        tank = Tank()
+        loadSettings()
+        tank!!.setGenomeFile(genomeFile)
+    }
 
-		newSaveDir();
-		loadMostRecentTank();
-		loadSettings();
-		RANDOM = new Random(Settings.simulationSeed);
-	}
+    fun setupTank() {
+        tank!!.initialise()
+    }
 
-	public Simulation(String name, String save)
-	{
-		simulate = true;
-		this.name = name;
-		genomeFile = "saves/" + name + "/genomes.csv";
-		historyFile = "saves/" + name + "/history.csv";
+    fun loadTank(filename: String) {
+        try {
+            tank = load(filename) as Tank
+            println("Loaded tank at: $filename")
+        } catch (e: IOException) {
+            println("Unable to load tank at " + filename + " because: " + e.message)
+            newDefaultTank()
+        } catch (e: ClassNotFoundException) {
+            println("Unable to load tank at " + filename + " because: " + e.message)
+            newDefaultTank()
+        }
+    }
 
-		newSaveDir();
-		loadTank("saves/" + name + "/tank/" + save);
-		loadSettings();
-		RANDOM = new Random(Settings.simulationSeed);
-	}
+    fun loadMostRecentTank() {
+        val dir = Paths.get("saves/$name/tank")
+        if (Files.exists(dir)) try {
+            Files.list(dir).use { pathStream ->
+                val lastFilePath = pathStream
+                    .filter { f: Path? -> !Files.isDirectory(f) }
+                    .max(Comparator.comparingLong { f: Path -> f.toFile().lastModified() })
+                lastFilePath.ifPresentOrElse(
+                    { path: Path -> loadTank(path.toString().replace(".dat", "")) }) { newDefaultTank() }
+            }
+        } catch (e: IOException) {
+            newDefaultTank()
+        } else newDefaultTank()
+    }
 
-	private void loadSettings() {
-		tank.cellCapacities.put(Protozoan.class, Settings.maxProtozoa);
-		tank.cellCapacities.put(PlantCell.class, Settings.maxPlants);
-		tank.cellCapacities.put(MeatCell.class, Settings.maxMeat);
-	}
+    fun simulate() {
+        setupTank()
+        makeHistorySnapshot()
+        while (simulate) {
+            if (pause) continue
+            if (delayUpdate && updateDelay > 0) {
+                val currTime = timeSeconds
+                if (currTime - lastUpdateTime > updateDelay) {
+                    update()
+                    lastUpdateTime = currTime
+                }
+            } else {
+                update()
+            }
+            if (tank!!.numberOfProtozoa() <= 0 && Settings.finishOnProtozoaExtinction) {
+                simulate = false
+                println()
+                println("Finished simulation. All protozoa died.")
+                printStats()
+            }
+        }
+    }
 
-	private void newSaveDir() {
-		try {
-			Path saveDir = Paths.get("saves/" + name);
-			if (!Files.exists(saveDir)) {
-				Files.createDirectories(saveDir);
-				Files.createDirectories(Paths.get("saves/" + name + "/tank"));
+    fun printStats() {
+        tank!!.getStats(true).forEach { (k: String?, v: Float?) -> System.out.printf("%s: %.5f\n", k, v) }
+    }
 
-				File original = new File(defaultSettingsPath);
-				File copied = new File(settingsPath);
-				com.google.common.io.Files.copy(original, copied);
-			}
+    fun update() {
+        val delta = timeDilation * Settings.simulationUpdateDelta
+        synchronized(tank!!) { tank!!.update(delta) }
+        timeSinceSave += delta
+        if (timeSinceSave > Settings.timeBetweenSaves) {
+            timeSinceSave = 0f
+            saveTank()
+        }
+        timeSinceSnapshot += delta
+        if (timeSinceSnapshot > Settings.historySnapshotTime) {
+            timeSinceSnapshot = 0f
+            makeHistorySnapshot()
+        }
+    }
 
-			Path genomePath = Paths.get(genomeFile);
-			if (!Files.exists(genomePath))
-				Files.createFile(genomePath);
+    fun close() {
+        simulate = false
+        println()
+        println("Closing simulation.")
+        saveTank()
+    }
 
-			Path historyPath = Paths.get(historyFile);
-			if (!Files.exists(historyPath))
-				Files.createFile(historyPath);
+    fun saveTank() {
+        val timeStamp = SimpleDateFormat("yyyy-MM-dd-HH-mm-ss").format(Date())
+        val fileName = "saves/$name/tank/$timeStamp"
+        save(tank, fileName)
+    }
 
-			String seedFile = "saves/" + name + "/seed.txt";
-			Path seedPath = Paths.get(seedFile);
-			if (!Files.exists(seedPath)) {
-				try {
-					Files.createFile(seedPath);
-					FileIO.appendLine(seedFile, Settings.simulationSeed + "");
-				} catch (IOException e) {
-					System.out.println("Failed to create seed file.");
-				}
-			}
+    fun makeHistorySnapshot() {
+        val stats = tank!!.getStats(true)
+        if (statsNames == null) {
+            statsNames = ArrayList(tank!!.getStats(true).keys)
+            val statsCsvHeader = java.lang.String.join(",", statsNames)
+            appendLine(historyFile, statsCsvHeader)
+        }
+        val statsString = statsNames!!.stream()
+            .map { k: String -> String.format("%.5f", stats[k]) }
+            .collect(Collectors.joining(","))
+        appendLine(historyFile, statsString)
+    }
 
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-	}
+    fun toggleDebug() {
+        debug = !debug
+    }
 
-	public static String generateSimName() {
-		Faker faker = new Faker();
-		return String.format("%s-%s-%s",
-				faker.ancient().primordial().toLowerCase().replaceAll(" ", "-"),
-				faker.pokemon().name().toLowerCase().replaceAll(" ", "-"),
-				faker.lorem().word().toLowerCase().replaceAll(" ", "-"));
-	}
-	
-	public void newDefaultTank()
-	{
-		tank = new Tank();
-		loadSettings();
-		tank.setGenomeFile(genomeFile);
-	}
+    fun togglePause() {
+        pause = !pause
+    }
 
-	public void setupTank() {
-		tank.initialise();
-	}
+    fun inDebugMode(): Boolean {
+        return debug
+    }
 
-	public void loadTank(String filename)
-	{
-		try {
-			tank = (Tank) FileIO.load(filename);
-			System.out.println("Loaded tank at: " + filename);
-		} catch (IOException | ClassNotFoundException e) {
-			System.out.println("Unable to load tank at " + filename + " because: " + e.getMessage());
-			newDefaultTank();
-		}
-	}
+    val generation: Long
+        get() = tank!!.generation
+    val elapsedTime: Float
+        get() = tank!!.elapsedTime
 
-	public void loadMostRecentTank() {
-		Path dir = Paths.get("saves/" + name + "/tank");
-		if (Files.exists(dir))
-			try (Stream<Path> pathStream = Files.list(dir)) {
-				Optional<Path> lastFilePath = pathStream
-						.filter(f -> !Files.isDirectory(f))
-						.max(Comparator.comparingLong(f -> f.toFile().lastModified()));
+    fun setUpdateDelay(updateDelay: Float) {
+        this.updateDelay = updateDelay.toDouble()
+    }
 
-				lastFilePath.ifPresentOrElse(
-						path -> loadTank(path.toString().replace(".dat", "")),
-						this::newDefaultTank
-				);
-			} catch (IOException e) {
-				newDefaultTank();
-			}
-		else newDefaultTank();
-	}
+    fun toggleUpdateDelay() {
+        delayUpdate = !delayUpdate
+    }
 
-	public void simulate() {
-		setupTank();
-		makeHistorySnapshot();
-		while (simulate) {
-			if (pause)
-				continue;
-
-			if (delayUpdate && updateDelay > 0) {
-				double currTime = Utils.getTimeSeconds();
-				if ((currTime - lastUpdateTime) > updateDelay) {
-					update();
-					lastUpdateTime = currTime;
-				}
-			} else {
-				update();
-			}
-
-			if (tank.numberOfProtozoa() <= 0 && Settings.finishOnProtozoaExtinction) {
-				simulate = false;
-				System.out.println();
-				System.out.println("Finished simulation. All protozoa died.");
-				printStats();
-			}
-		}
-	}
-
-	public void printStats() {
-		tank.getStats(true).forEach(
-			(k, v) -> System.out.printf("%s: %.5f\n", k, v)
-		);
-	}
-
-	public void update()
-	{
-		float delta = timeDilation * Settings.simulationUpdateDelta;
-		synchronized (tank) {
-			tank.update(delta);
-		}
-
-		timeSinceSave += delta;
-		if (timeSinceSave > Settings.timeBetweenSaves) {
-			timeSinceSave = 0;
-			saveTank();
-		}
-
-		timeSinceSnapshot += delta;
-		if (timeSinceSnapshot > Settings.historySnapshotTime) {
-			timeSinceSnapshot = 0;
-			makeHistorySnapshot();
-		}
-	}
-
-	public void close() {
-		simulate = false;
-		System.out.println();
-		System.out.println("Closing simulation.");
-		saveTank();
-	}
-
-	public void saveTank() {
-		String timeStamp = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss").format(new java.util.Date());
-		String fileName = "saves/" + name + "/tank/" + timeStamp;
-		FileIO.save(tank, fileName);
-	}
-
-	public void makeHistorySnapshot() {
-		Map<String, Float> stats = tank.getStats(true);
-
-		if (statsNames == null) {
-			statsNames = new ArrayList<>(tank.getStats(true).keySet());
-			String statsCsvHeader = String.join(",", statsNames);
-			FileIO.appendLine(historyFile, statsCsvHeader);
-		}
-
-		String statsString = statsNames.stream()
-				.map(k -> String.format("%.5f", stats.get(k)))
-				.collect(Collectors.joining(","));
-
-		FileIO.appendLine(historyFile, statsString);
-	}
-
-	public void toggleDebug() {
-		debug = !debug;
-	}
-
-	public void togglePause() {
-		pause = !pause;
-	}
-
-	public boolean inDebugMode() {
-		return debug;
-	}
-
-	public Tank getTank() { return tank; }
-
-	public long getGeneration() { return tank.getGeneration(); }
-
-	public float getElapsedTime() { return tank.getElapsedTime(); }
-
-	public float getTimeDilation() { return timeDilation; }
-
-	public void setTimeDilation(float td) { timeDilation = td; }
-
-	public void setUpdateDelay(float updateDelay) {
-		this.updateDelay = updateDelay;
-	}
-
-	public void toggleUpdateDelay() {
-		delayUpdate = !delayUpdate;
-	}
+    companion object {
+        var defaultSettingsPath = "config/default_settings.yaml"
+        @JvmField
+		var settingsPath = defaultSettingsPath
+        lateinit var RANDOM: Random
+        fun generateSimName(): String {
+            val faker = Faker()
+            return String.format(
+                "%s-%s-%s",
+                faker.ancient().primordial().lowercase(Locale.getDefault()).replace(" ".toRegex(), "-"),
+                faker.pokemon().name().lowercase(Locale.getDefault()).replace(" ".toRegex(), "-"),
+                faker.lorem().word().lowercase(Locale.getDefault()).replace(" ".toRegex(), "-")
+            )
+        }
+    }
 }
